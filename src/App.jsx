11c472c68df93c5eba117f9771c2e154165ec8bd
Luana Oliveira <lua.aniceto@gmail.com>
2021-05-05 16:29:39 -0300

    Inclusao tela lista veiculos

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Header from "./components/organisms/Header.js"
 import Authentication from "./pages/authentication/Authentication.js"
 import ListOfCars from "./pages/cars/List.js"
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
@@ -5,12 +6,19 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-        <Router>
-            <Switch>
-              <Route path='/login' component={ Authentication } />
-              <Route path='/' component={ ListOfCars } exact />
-            </Switch>
-        </Router>
+      <Router>
+        <div className="App">
+        <Header />
+        </div>
+          <Switch>
+            <Route path="/login">
+              <Authentication />
+            </Route>
+            <Route path="/">
+              <ListOfCars />
+            </Route>
+          </Switch>
+      </Router>
   );
 }
 
--- src/pages/cars/List.js
+++ src/pages/cars/List.js
@@ -1,24 +1,29 @@
-import * as React from 'react';
+import React from 'react';
 import { DataGrid } from '@material-ui/data-grid';
-import { useDemoData } from '@material-ui/x-grid-data-generator';
+import Grid from '@material-ui/core/Grid';
 
-export default function BasicFilteringGrid() {
-  const { data } = useDemoData({
-    dataSet: 'Commodity',
-    rowLength: 100,
-    maxColumns: 6,
-  });
+const rows = [
+  { id: 1, marca: 'Fiat', modelo: 'Ford KA', ano: '2016', valor: '15.000,00' },
+  { id: 2, marca: 'Fiat', modelo: 'Ford KA', ano: '2020', valor: '25.000,00' }
+];
 
+const columns = [
+  { field: 'marca', headerName: 'Marca', width: 150 },
+  { field: 'modelo', headerName: 'Modelo', width: 150 },
+  { field: 'ano', headerName: 'Ano', width: 150 },
+  { field: 'valor', headerName: 'Valor', width: 150 },
+];
+
+export default function App() {
   return (
-    <div style={{ height: 400, width: '100%' }}>
-      <DataGrid
-        {...data}
-        filterModel={{
-          items: [
-            { columnField: 'commodity', operatorValue: 'contains', value: 'rice' },
-          ],
-        }}
-      />
-    </div>
+    <Grid container spacing={3}>
+      <Grid item xs={2}> <div/></Grid>
+      <Grid item xs={12} lg={8} >
+        <div style={{ height: 300, width: '100%' }}>
+          <DataGrid rows={rows} columns={columns} />
+        </div>
+       </Grid>
+       <Grid item xs={2}> <div/></Grid>
+    </Grid>
   );
 }
