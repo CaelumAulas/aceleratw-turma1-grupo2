02dd105156c317f97ae5cb1f0e96f144cd5cdfc8
Luana Oliveira <lua.aniceto@gmail.com>
2021-05-14 20:15:11 -0300

    ajuste teste

--- src/test/components/organisms/Authentication.test.js
+++ src/test/components/organisms/Authentication.test.js
@@ -1,21 +1,35 @@
 import React from 'react'
 import Authentication from '../../../components/organisms/Authentication.jsx'
 import { render, screen }  from '@testing-library/react'
+import { MemoryRouter } from "react-router-dom"
+
 describe("Authentication", () => {
  
   describe("Test if fields exists", () => {
     it("email should exist", () => {
-      render(<Authentication />)
+      render(
+        <MemoryRouter initialEntries={["/cadastro-marca"]}>
+          <Authentication />
+        </MemoryRouter>
+        )
       expect(screen.getByTestId('email')).toBeInTheDocument()
     })
 
     it("password should exist", () => {
-      render(<Authentication />)
+      render(
+        <MemoryRouter initialEntries={["/cadastro-marca"]}>
+          <Authentication />
+        </MemoryRouter>
+        )
       expect(screen.getByTestId('password')).toBeInTheDocument()
     })
 
     it("btnClick should exist", () => {
-      render(<Authentication />)
+      render(
+        <MemoryRouter initialEntries={["/cadastro-marca"]}>
+          <Authentication />
+        </MemoryRouter>
+        )
       expect(screen.getByTestId('btnClick')).toBeInTheDocument()
     })
   })
